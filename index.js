80170157c2df62536129cc26be46e3f14cae5254
Seth Etter <sethetter@gmail.com>
2015-10-15 10:11:27 -0500

    Get most recent tweet ID upon launch
    We pass this as a `since_id` when requesting statuses so no backlog flooding
    will happen.

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ var INCLUDE_RTS = process.env.INCLUDE_RTS || false;
 var INCLUDE_MENTIONS = process.env.INCLUDE_MENTIONS || false;
 var CHECK_INTERVAL = process.env.CHECK_INTERVAL || 300000;
 
+var MOST_RECENT_TWEET_ID;
+
 // Strip any possible spaces left from comma separation
 ACCOUNTS_TO_RT.forEach((account, i) => {
   ACCOUNTS_TO_RT[i] = account.trim();
@@ -24,13 +26,27 @@ var client = new Twitter({
   access_token_secret:process.env.ACCESS_TOKEN_SECRET
 });
 
+function getMostRecentStatusID() {
+
+  client.get('statuses/home_timeline', {
+    count: 1
+  }, (err, tweets, response) => {
+    if (err) throw err;
+    MOST_RECENT_TWEET_ID = tweets[0].id_str;
+    console.log(`Set most recent tweet ID to prevent backlog: ${MOST_RECENT_TWEET_ID}`);
+  });
+}
+
 // Get a list of most recent statuses for an account
 function getStatusesForAccount(username) {
+  console.log(`Grabbing statuses for ${username}`);
+
   client.get('statuses/user_timeline', {
     screen_name: username,
     exclude_replies: !INCLUDE_MENTIONS,
     include_rts: INCLUDE_RTS,
-    count: 10
+    count: 10,
+    since_id: MOST_RECENT_TWEET_ID
   }, (err, tweets, response) => {
     if (err) throw err;
     processStatuses(tweets)
@@ -40,6 +56,7 @@ function getStatusesForAccount(username) {
 // Check each status for already RT'd, and RT if not already
 function processStatuses(statuses) {
   statuses.forEach((status) => {
+    console.log(`-- Processing status ${status.id_str}`);
     // Don't RT if we already have
     if (!status.retweeted) retweetStatus(status.id_str);
   });
@@ -50,10 +67,13 @@ function retweetStatus(statusID) {
     id: statusID
   }, (err, retweetedStatus, response) => {
     if (err) throw err;
+    console.log(`-- Retweeting status ${status.id_str} from ${retweetStatus.screen_name}`);
   });
 }
 
 // Do it!
+getMostRecentStatusID();
+
 setInterval(() => {
   ACCOUNTS_TO_RT.forEach(getStatusesForAccount)
 }, CHECK_INTERVAL);
